01faea02dbaf28a5ac429abde18d73ef578ebfc0
facikal <fadhil1999@gmail.com>
2022-08-17 22:17:54 +0700

    merubah navbar dan merubah ukuran me.img

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Nav, Navbar } from "react-bootstrap";
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
 import { Link, useLocation } from "react-router-dom";
 import "./navbar.css";
 
@@ -14,16 +15,32 @@ const Navigation = () => {
     console.log(location.pathname)
     return (
         <React.Fragment>
-                <Navbar bg="dark" variant="dark">
-                    <Container>
-                        <Navbar.Brand>Ya Jadi Gitu Cuy..</Navbar.Brand>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Ya Jadi Gitu Cuy..</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
                             <Nav.Link as={Link} to={"/"} className={activeNav === "/" ? "text-info" : " "}>Homepage</Nav.Link>
                             <Nav.Link as={Link} to={"/gallery"} className={activeNav === "/gallery" ? "text-info" : " "}>Gallery</Nav.Link>
                             <Nav.Link as={Link} to={"/posts"} className={activeNav === "/posts" ? "text-info" : " "}>Posts</Nav.Link>
                         </Nav>
-                    </Container>
-                </Navbar>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            {/* <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand>Ya Jadi Gitu Cuy..</Navbar.Brand>
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to={"/"} className={activeNav === "/" ? "text-info" : " "}>Homepage</Nav.Link>
+                            <Nav.Link as={Link} to={"/gallery"} className={activeNav === "/gallery" ? "text-info" : " "}>Gallery</Nav.Link>
+                            <Nav.Link as={Link} to={"/posts"} className={activeNav === "/posts" ? "text-info" : " "}>Posts</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar> */}
         </React.Fragment>
     )
 }
